06d0203c59d180d3b4b6801418c6e5310276656c
Juan López Rubio <j.lopez.r@gmail.com>
2016-09-11 15:53:26 +0200

    first commit

---
+++ add-jira-db.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE USER jira WITH PASSWORD 'jira';
+    CREATE DATABASE jira WITH ENCODING 'UNICODE' LC_COLLATE 'C' LC_CTYPE 'C' TEMPLATE template0;
+    GRANT ALL PRIVILEGES ON DATABASE jira TO jira;
+EOSQL
